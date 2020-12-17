90f4377da84a2f2bd47603d4b4ee79e5340203fe
nganvn <nganvn2110@gmail.com>
2020-12-17 21:04:20 +0700

    add medicine

--- project/static/handleEvents.js
+++ project/static/handleEvents.js
@@ -1,27 +1,41 @@
-$('table').on('click', '.delete-item', function() {
+const medicineTable = $('#medicine-table')
+const medicineBody = $('#medicine-body')
+
+medicineTable.on('click', '.delete-item', function() {
   $(this).closest('tr').remove()
+  const myBody = medicineTable.find('tbody')
+  index = 1
+  myBody.find('strong').each(function () {
+    this.innerHTML = index++
+  })
 })
 
-$('.table > tbody').on('click', '.add-item', function() {
+$('#add-item').click(function() {
+  const myBody = medicineTable.find('tbody')
   const innerHTML = `
   <tr>
-    <td><strong>1</strong></td>
-    <td>
-        <div class="control">
-            <input class="input" type="text" placeholder="Small input">
-        </div>
-    </td>
-    <td>
-        <div class="control">
-            <input class="input" type="text" placeholder="Small input">
-        </div>
-    </td>
-    <td>
-        <button class="button is-danger is-inverted">
-            <i class="fas fa-minus"></i>
-        </button>
-    </td>
+      <td><strong>${myBody.find('tr').length+1}</strong></td>
+      <td>
+          <div class="control">
+              <input class="input" type="text" placeholder="Small input">
+          </div>
+      </td>
+      <td>
+          <div class="control">
+              <input class="input" type="text" placeholder="Small input">
+          </div>
+      </td>
+      <td>
+          <button class="button is-danger is-inverted delete-item">
+              <i class="fas fa-minus"></i>
+          </button>
+      </td>
   </tr>
   `
-  $this.append(innerHTML)
+  const item = jQuery.parseHTML(innerHTML);
+  $(item).on('click', '.delete-item', function() {
+    $(this).closest('tr').remove()
+  })
+  myBody.append(item)
+  medicineBody.scrollTop(medicineBody.height()+9999)
 })
