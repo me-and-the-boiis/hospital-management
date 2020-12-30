c67e719c82bd4d8c36498bd9b19958dde010c8d5
nganvn <nganvn2110@gmail.com>
2020-12-30 23:48:15 +0700

    add doctor rrequest

--- project/doctor.py
+++ project/doctor.py
@@ -1,3 +1,5 @@
+import datetime
+
 from flask import *
 from . import db
 
@@ -7,37 +9,31 @@ cursor = conn.cursor()
 
 @doctor.route('/doctor', methods=['GET', 'POST', 'PUT'])
 def doctorWorkspace():
-    pHin = ''
-    if request.method == 'POST':
-        myhin   = request.form['hin']
-        # cursor.execute('SELECT * FROM patient WHERE hin LIKE %s', myhin)
-        # data = cursor.fetchall()
-        # pHin = data[0][0]
-        # return render_template('Doctor/index.html', data=data)
-        return redirect(url_for("doctor.doctorWorkspace", patientHIN=myhin))
+    if request.method == 'GET':
+        myHin = ""
+        try:
+            myHin = request.args['hin']
+            cursor.execute('SELECT * FROM patient WHERE hin LIKE %s', myHin)
+            data = cursor.fetchall()
+            return render_template('Doctor/index.html', data=data[0], hin = myHin)
+        except:
+            return render_template('Doctor/index.html', data=None, hin = myHin)
 
-    if request.method == 'PUT':
+    if request.method == 'POST':
+        currentDateTime = datetime.datetime.now()
         note    = request.form['note']
         test    = request.form['test']
         xray    = request.form['xray']
         ptype   = request.form['ptype']
         diagnosis = request.form['diagnosis']
-        surgery = request.form['surgery']
+        # surgery = request.form['surgery']
         date    = request.form['date']
         doctor = 10007
-        pHin = request.args['patientHIN']
-        # cursor.execute(
-        #     'CALL Insert_Prescription(\'{}\', \'{}\', \'{}\', \'{}\', \'{}\', \'{}\', \'{}\', \'{}\', \'{}\')'.format(date, doctor, pHin, test, xray, diagnosis, ptype, date, note)
-        # )
-        print(date, doctor, pHin, test, xray, diagnosis, ptype, date, note)
-    try:
-        myHin = request.args['patientHIN']
-    except:
-        return render_template('Doctor/index.html', data=(None,))
-    cursor.execute('SELECT * FROM patient WHERE hin LIKE %s', myHin)
-    data = cursor.fetchall()
-    if not data:
-        data = (None,)
-
-    return render_template('Doctor/index.html', data = data)
-
+        pHin = request.args['hin']
+        cursor.execute(
+            'CALL Insert_Prescription(\'{}\', \'{}\', \'{}\', \'{}\', \'{}\', \'{}\', \'{}\', \'{}\', \'{}\')'.format(currentDateTime, doctor, pHin, test, xray, diagnosis, ptype, date, note)
+        )
+        conn.commit()
+        flash('Thêm đơn thuốc thành công bệnh nhân {}!'.format(pHin))
+        # print(currentDateTime, doctor, pHin, test, xray, diagnosis, bool(ptype), date, note)
+        return redirect(url_for("doctor.doctorWorkspace", hin=pHin))
